6085e9aac4e44fe5fbf7da1434af15490eed6fe7
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2022-10-21 17:59:42 +0200

    feat: update footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,14 +1,6 @@
-import styles from './Footer.module.css';
-
 const Footer = () => (
-	<footer className={styles.footer}>
-		<a
-			href="https://github.com/TempoJC"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Powered by <span className={styles.name}>@TempoJC</span>
-		</a>
+	<footer className="flex justify-between px-4 md:px-20 py-8">
+		<span>&copy; {new Date().getFullYear()} Jose Luis Camargo</span>
 	</footer>
 );
 
