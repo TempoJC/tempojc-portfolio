44963e281271b21697c65df3753e613f6573c9a8
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2022-10-21 10:04:22 +0200

    chore: update jest.config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,23 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
 	moduleDirectories: ['node_modules', __dirname],
+	moduleNameMapper: {
+		// Components
+		'^@/Atoms(.*)$': '<rootDir>/src/components/atoms$1',
+		'^@/Molecules(.*)$': '<rootDir>/src/components/molecules$1',
+		'^@/Organisms(.*)$': '<rootDir>/src/components/organisms$1',
+		'^@/Templates(.*)$': '<rootDir>/src/components/templates$1',
+		// Other folders
+		'^@/Constants(.*)$': '<rootDir>/src/constants$1',
+		'^@/types(.*)$': '<rootDir>/@types$1',
+		'^@/Config(.*)$': '<rootDir>/src/config$1',
+		'^@/Mocks(.*)$': '<rootDir>/src/mockData$1',
+		'^@/Pages(.*)$': '<rootDir>/src/pages$1',
+		'^@/Public(.*)$': '<rootDir>/public$1',
+		'^@/Utils(.*)$': '<rootDir>/src/utils$1',
+		'^@/Hooks(.*)$': '<rootDir>/src/hooks$1',
+		'^@/State(.*)$': '<rootDir>/src/state$1',
+	},
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	// Add more setup options before each test is run
 	// setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
