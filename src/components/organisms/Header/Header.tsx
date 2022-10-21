e9722f9d69e46f0def8f2afb9e810c9b0180944c
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2022-10-21 19:13:14 +0200

    test: update header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { NAV_ITEMS } from './Header.constants';
+import { Logo } from '@/Atoms/Logo';
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -24,30 +23,20 @@ const Header = () => {
 
 	return (
 		<>
-			<header className="fixed w-full z-50 px-4 lg:px-20 py-4">
+			<header
+				className="fixed w-full z-50 px-4 lg:px-20 py-4"
+				data-testid="header"
+			>
 				<div className="flex justify-between items-center">
-					<Link href="/" passHref>
-						<motion.a
-							className="group relative transition whitespace-nowrap hover:transition duration-300 text-2xl font-bold"
-							animate={{ opacity: 1, y: 0 }}
-							initial={{ opacity: 0, y: -10 }}
-						>
-							<span className="transition duration-300 opacity-100 group-hover:opacity-0">
-								@TempoJC
-							</span>
-							<span className="absolute transition duration-300 left-0 opacity-0 group-hover:opacity-100 text-transparent bg-clip-text hover:bg-gradient-to-r from-light-start to-light-stop dark:from-dark-start dark:to-dark-stop">
-								Jose Luis Camargo
-							</span>
-						</motion.a>
-					</Link>
+					<Logo />
 
 					<nav className="hidden md:block">
 						<ul className="flex gap-8 text-lg">
 							{NAV_ITEMS.map(({ hash, title }, index) => (
-								<>
+								<li key={`${title}-${hash}-${index} `}>
 									{/* NavigationItem */}
-									{`${title}-${hash}-${index} `}
-								</>
+									{title}
+								</li>
 							))}
 						</ul>
 					</nav>
