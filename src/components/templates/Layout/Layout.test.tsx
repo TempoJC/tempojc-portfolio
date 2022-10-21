64b7c7b9b9d95cf5e1a12256cda6530a31bf7277
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2022-10-21 19:12:23 +0200

    test: update layout

--- src/components/templates/Layout/Layout.test.tsx
+++ src/components/templates/Layout/Layout.test.tsx
@@ -1,5 +1,16 @@
+import { useRouter } from 'next/router';
 import { render } from '@testing-library/react';
 import { Layout } from '../Layout';
+
+jest.mock('next/router', () => ({
+	__esModule: true,
+	useRouter: jest.fn().mockReturnValue({
+		route: '',
+		pathname: '',
+		query: '',
+		asPath: '',
+	}),
+}));
 
 describe('Layout', () => {
 	it('renders correctly', () => {
